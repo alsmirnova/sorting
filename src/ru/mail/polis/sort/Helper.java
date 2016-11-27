c7193b700e21551aec126dd772db1ee677738eb7
alsmirnova <alyona_smir@mail.ru>
2016-11-27 23:45:52 +0300

    Update Helper.java

--- src/ru/mail/polis/sort/Helper.java
+++ src/ru/mail/polis/sort/Helper.java
@@ -24,4 +24,50 @@ public class Helper {
         }
         return a;
     }
+
+    public static int[] order(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+        return a;
+    }
+
+    public static int[] disorder(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = a.length-1-i;
+        }
+        return a;
+    }
+
+    public static int[] qsortcase(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = i + 1;
+        }
+        for (int i = 0; i<n; i++) {
+            swap(a,i,i/2);
+
+        }
+        int i1=-1,i2=-1;
+        for (int i=0;i<n;i++) {
+            if (a[i]==1) i1=i;
+            else if (a[i]==2) i2=i;
+            if (i1!=-1 && i2!=-1) break;
+        }
+        if (n>2)swap(a,i1,i2);
+        return a;
+
+    }
+
+    public static int[] repeatkeys(int n) {
+        int[] a=new int[n];
+        for (int i=0; i<a.length-1; i+=2) {
+            a[i]=r.nextInt(10);
+            a[i+1]=i;
+        }
+        a[a.length-1]=a[0];
+        return a;
+    }
 }
